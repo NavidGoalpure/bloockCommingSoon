70879b180163b94b0814442f29822e79f1b1a203
Navid Goalpure <Navid.Goalpure@gmail.com>
2018-12-01 11:46:23 +0330

    change setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Bloock",
+    author: "نوید گل پور",
+    description: "اجاره کتاب بر بستر بلاکچین"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
