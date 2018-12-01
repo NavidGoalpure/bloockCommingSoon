edfd197ca9add683a64542d9e68f63ccca4fe55b
Navid Goalpure <Navid.Goalpure@gmail.com>
2018-12-01 16:32:40 +0330

    improve rtl and persian language

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,19 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Bloock</h1>
+                <p>بلووک یک پلتفرم اجازه کتابه که بر بستر بلاکچین فعالیت میکنه. توی بلووک شما میتونید 
+                    کتاب‌هاتون رو اجاره بدید یا کتاب‌های دیگران رو اجاره کنید.
+                    با این کار هم یک صرفه جویی ۸۰ درصدی تو خرید کتاب اتفاق می‌افته،‌هم درختای کمتری قطع میشه
+                    برای اطلاعات بیشتر درباره بلاکچین و تاثیر اون تو جامعه به وبلاگ MrNawid مراجعه کنین.  <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>پروژه</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>چرا بلاکچین</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>درباره ما</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>لیست انتظار</a></li>
             </ul>
         </nav>
     </header>
