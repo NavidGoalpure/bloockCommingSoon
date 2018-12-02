dfb7aefbc4cabd6f77974f1129928688ad4de338
Navid Goalpure <Navid.Goalpure@gmail.com>
2018-12-02 16:24:39 +0330

    some

--- src/components/Main.js
+++ src/components/Main.js
@@ -177,7 +177,12 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#" data-netlify="true">
+          <form
+            name="bloock-contact"
+            method="post"
+            action="#"
+            data-netlify="true"
+          >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
