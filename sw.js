d7f90a8321a1e4a839f723eb9a3a20405d705e0d
Navid Goalpure <Navid.goalpure@gmail.com>
2019-01-07 14:54:30 +0330

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6d65620fab060ac2d628.js"
+    "url": "webpack-runtime-cfe5fcb96f23ec61d7bf.js"
   },
   {
     "url": "app-9023f9d37ed066a50c08.js"
@@ -36,13 +36,13 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d5aff3e010d2f00c68dd57a730b06aa8"
+    "revision": "6ee0a51f3d5e9755ff49ea0c399f5543"
   },
   {
     "url": "1.24fa41d24af8a19e7868.css"
   },
   {
-    "url": "1-0765a9796622b0332caa.js"
+    "url": "1-3afe0a3009f29e807f0e.js"
   },
   {
     "url": "component---src-pages-404-js-38126362def42917f031.js"
